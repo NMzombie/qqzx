3a7a028668f9b129b64aa746db4e7b2ff8b57889
openresty <18375335508@163.com>
2019-01-26 16:18:12 +0800

    修改

--- manage/js/content.js
+++
@@ -1,1 +0,0 @@
-function animationHover(element,animation){element=$(element);element.hover(function(){element.addClass('animated '+animation);},function(){window.setTimeout(function(){element.removeClass('animated '+animation);},2000);});}
