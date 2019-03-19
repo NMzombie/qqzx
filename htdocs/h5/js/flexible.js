3fc23406ef67007774e6facac90c5454354ee2f1
薛镜池 <994022632@qq.com>
2019-03-19 20:12:07 +0800

    rankling detail repair

--- htdocs/h5/js/flexible.js
+++ htdocs/h5/js/flexible.js
@@ -18,7 +18,6 @@
   var d, e = a.document, f = e.documentElement, g = e.querySelector('meta[name="viewport"]'),
     h = e.querySelector('meta[name="flexible"]'), i = 0, j = 0, k = b.flexible || (b.flexible = {});
   if (g) {
-    console.warn("将根据已有的meta标签来设置缩放比例");
     var l = g.getAttribute("content").match(/initial\-scale=([\d\.]+)/);
     l && (j = parseFloat(l[1]), i = parseInt(1 / j))
   } else if (h) {
