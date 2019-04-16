2107ff12353c018a292289d39d5f1c5681786ae5
wangxiaopeng <807933401@qq.com>
2019-04-16 13:50:51 +0800

    优惠券提示

--- htdocs/platform/admin/coupon/couponmanage/couponItemsEdit.js
+++ htdocs/platform/admin/coupon/couponmanage/couponItemsEdit.js
@@ -936,10 +936,11 @@ $(function(){
 	    	url: "/admin/activity/couponcommanage/givingCouponCom.json?couponComIds=" + list_couponComId +"&mid=" + $('#memberCoupon').val()+"&depositId="+ + $('#depositId').val()+"&requestNo="+$('#requestNo').val(),
             type: "get",
             success: function (data) {
+            	
                 if (data != null&&data.success) {
                 	swal('赠送成功!');
                 }else{
-                	swal('赠送失败!')
+                	swal('赠送失败!'+data.errorInfo)
                 }
                 $('#myModal').hide();
             }
