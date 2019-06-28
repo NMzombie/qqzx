2c21fda402f01bad41cb092a7aad17608f4f620b
KZY <704342956@qq.com>
2019-06-28 15:35:36 +0800

    新版官网

---
+++ htdocs/h5/h5mall/js/home-info.js
@@ -0,0 +1,36 @@
+$(function () {
+    $.ajax({
+        url: "js/info.json",
+        type: "GET",
+        dataType: "json",
+        success: (res) => {
+            const data = res.data;
+            let a=0;
+            // 模块1
+            let contentTop = '';
+            contentTop += `<div class="headerline"><div class="line"></div> <div class="title">${data[a].moduleView.moduleName}</div> </div> <div class="bottom-line"></div><div class="videoHot"><ul></ul></div><div class="spacer"></div>`;
+            $('.mode1').append(contentTop);
+            let content='';
+            for(let i=0;i<data[a].moduleViewInfoList.length;i++){
+                content += `<li><div class="cover"><img class="cover-img" lazy="loaded" src="${data[a].moduleViewInfoList[i].imageUrl}"></div><div class="videoInfo"><h4>${data[a].moduleViewInfoList[i].title}</h4> <p class="desc">${data[a].moduleViewInfoList[i].titleDesc}</p><p class="price"></p></div></li>`
+            }
+            $('.videoHot ul').append(content)
+
+
+            // 模块2
+            contentTop = '';
+            a++;
+            console.log(a)
+            contentTop += `<div class="headerline"><div class="line"></div><div class="title">印象塑造</div><div class="more"><span>查看全部</span></div></div><div class="bottom-line"></div><div class="studyPlan"><ul class="vipul"></ul><div class="spacer"></div>`;
+            $('.mode2').append(contentTop);
+            content='';
+            for(let i=0;i<data[a].moduleViewInfoList.length;i++){
+                content += `<li><div class="cover"><img class="cover-img" src="https://img.mall.xc2018.com.cn/mall/upload/20190625/173116_33_0ywi.jpg" lazy="loaded"></div><div class="classInfo"><h4>如何塑造第一印象</h4><p>头像和昵称</p><p class="desc"><span class="desc1">已经有5人学习</span><span class="desc2">1讲|2课</span></p></div></li>`
+            }
+            $('.mode2 .vipul').append(content)
+
+
+        }
+    })
+
+});
