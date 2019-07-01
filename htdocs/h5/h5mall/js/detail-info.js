7530002c0aaeab0be8e44c87237328de5d49711e
康梓杨 <704342956@qq.com>
2019-07-01 17:02:42 +0800

    新版官网2

--- htdocs/h5/h5mall/js/detail-info.js
+++ htdocs/h5/h5mall/js/detail-info.js
@@ -11,7 +11,8 @@ $(function () {
             for(let i=0;i<data.list.length;i++){
                 if(getQueryString('id') === JSON.stringify(data.list[i].id)){
                     topImg +=`<img src="${JSON.parse(data.list[i].gallery)[0].img}" alt="">`;
-                    content += `<h4>${data.list[i].name}</h4><div class="priceInfo"><span class="price vipFree tag">会员免费</span></div><div class="share"><i class="iconfont">&#xe624;</i><span> 分享</span></div><div class="buyer">${data.list[i].listenCountDesc}</div>`
+                    content += `<h4>${data.list[i].name}</h4><div class="priceInfo"><span class="price vipFree tag">会员免费</span></div><div class="buyer">${data.list[i].listenCountDesc}</div>`;
+                    // content += `<h4>${data.list[i].name}</h4><div class="priceInfo"><span class="price vipFree tag">会员免费</span></div><div class="share"><i class="iconfont">&#xe624;</i><span> 分享</span></div><div class="buyer">${data.list[i].listenCountDesc}</div>`
                     contentImg +=`${data.list[i].detail}`;
                 }
             }
